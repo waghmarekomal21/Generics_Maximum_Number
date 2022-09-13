683b72b5642e1da05510041e42df143d7493b83f
Komal <waghmarekomal21@gmail.com>
2022-09-13 18:56:55 +0530

    [Komal] Maximum Number Between Three Integer

---
+++ Max_Num.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Maximum
+{
+    public class Max_Num
+    {
+        public int MaxNum()
+        {
+
+            Console.WriteLine("Enter First Integer number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Integer number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Third Integer number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+
+
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+                 num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return num1; 
+
+        }
+
+    }
+}
